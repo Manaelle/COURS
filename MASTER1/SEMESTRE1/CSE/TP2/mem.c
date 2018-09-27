ef6bfb97b7c41820e25dc1e78c7031c57f7bde6e
Manal Benaissa <benaissm@F201-01.e-im2ag.ujf-grenoble.fr>
2018-09-27 17:01:04 +0200

    AJOUT : mem_free()

--- MASTER1/SEMESTRE1/CSE/TP2/mem.c
+++ MASTER1/SEMESTRE1/CSE/TP2/mem.c
@@ -99,14 +99,9 @@ fb* mem_fit_first(fb* T, size_t size){
 							AP->next = newFB;
 						}
 				}
-				else{
-					//A COMPLETER : Cas où pas de place pour FB
-				}
-				
 				
 			}
-			
-			
+
 			return AC;
 		}
 	
@@ -122,18 +117,79 @@ void mem_fit(mem_fit_function_t* f){
 void* mem_alloc(size_t S) {
 	
 	fb * T = (fb *)M.mem;
-	fb * AC = T; //Le FB actuel
-	fb * AP = T; //le FB précédent
+	ub * newU; //La future zone à allouer
 	
+	//ETAPE 1 ET 2 : Chercher une zone libre et remanipuler la liste chainée.
+	newU = (ub*)mem_fit(&mem_fit_first); //retourne l'adresse où écrire newU
 	
-	mem_fit(&mem_fit_first);
+	//ETAPE 3 : On crée UB
+	newU -> taille = S;
+	return ((void*)newU) + sizeof(ub); //On retourne le DEBUT de la zone allouée, UB exclue ! Il ne faut pas laisser l'occasion à l'utilisateur d'écraser UB...
 	
+}
+//-----------------------------------------
+//Ma fonction magique. Elle défragmente un peu tout ce foutoir en fusionnant les zones libres contigues.
+//J'aimerais bien qu'elle détecte aussi les zones "mortes" qui ne sont ni libres ni utilisés, les résidus en sommes) et me fasse un petit rapport. Un vrai déframenteur quoi !
+void ElDefragmentor(){
 	
-	return NULL;
+	fb * T = (fb *)M.mem;
+	fb* AC = T; //Le FB actuel
+	fb* AP = T; //le FB précédent
+	
+	size_t sizeFB;
+	
+	while(AC != NULL){
+			
+			sizeFB = AC->taille + sizeof(fb);
+			
+			AP = AC;
+			AC = AC->next;
+		}
 }
 //-----------------------------------------
 void mem_free(void* m){
-
+	
+	ub* U;
+	fb* newFB;
+	
+	//ETAPE 1 : Chercher le (vrai) début de la zone à libérer, cad avec UB
+	U = (ub*)(m-sizeof(ub));
+	
+	//ETAPE 2 : Création du nouveau FB
+	newFB = (fb*)U;
+	newFB -> taille = (U->taille) + sizeof(ub) - sizeof(fb);
+	newFB -> next = NULL;
+	
+	//ETAPE 3 : Vérifier si il existe une zone libre avant et/ou après newFB, histoire de fusionner
+	fb * T = (fb *)M.mem;
+	fb* AC = T; //Le FB actuel
+	fb* AP = T; //le FB précédent
+	size_t sizeAC;
+	
+	while((AC != NULL)&&((AC < newFB)){ //on cherche la zone libre JUSTE avant newFB
+			AP = AC;
+			AC = AC->next;
+		}
+	
+	//Normalement, à la fin, newFB se trouve entre AC et AP. Il faudra alors vérifier si on peut fusionner
+	if(AC != NULL){
+	
+		//Cas où on peut fusionner avec AC
+		if (newFB + (newFB->taille) + sizeof(fb) == AC){ 
+			newFB->taille = (newFB->taille) + (AC->taille) + sizeof(fb);
+			AP->next = newFB;	
+		}
+		
+		//Cas où on peut fusionner avec AP
+		if (AP + (AP->taille) + sizeof(fb) == newFB){ 
+			AP->taille = (AP->taille) + (newFB->taille) + sizeof(fb);	
+		}
+		
+		if(AC==T){
+			//A COMPLETER
+		}
+	}
+	
 }
 //-----------------------------------------
 size_t mem_get_size(void *m){
