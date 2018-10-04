d5fe0972e6c3823167a65fa372b9b61949eb9d3f
Manal Benaissa <benaissm@im2ag-mandelbrot.e.ujf-grenoble.fr>
2018-10-04 22:37:00 +0200

    FINI ET TEST : OK

--- MASTER1/SEMESTRE1/CSE/TP2/mem.c
+++ MASTER1/SEMESTRE1/CSE/TP2/mem.c
@@ -5,6 +5,8 @@
 #include <stddef.h>
 #include "mem.h"
 
+#define LIBRE 1
+#define OCCUPEE 0
 //-----------------------------------------
 //--------------- DONNEES -----------------
 //-----------------------------------------
@@ -42,13 +44,14 @@ void mem_init(char* mem, size_t taille){
 	fb * T = ((fb *)M.mem)+1; //T est le pointeur vers fb, qui sera stocké en mem[0]
 	fb **mem0 = (fb **)M.mem; //mem0 est pareil que mem mais casté en fb** (et non en char*)
 	*mem0 = T;// idem que mem[0] = T mais sans problème de cast
-	T->taille = taille - sizeof(fb *) - sizeof(fb) ;
+	T->taille = taille - sizeof(fb *) ; //taille de zone libre, FB compris
 	T->next = NULL;
 	
 	fonction_recherche = &mem_fit_first;
 	
 	printf("---------------------------------\n");
-	printf("INITIALISATION :\n Adresse de T : %p \n Nombre de zones libres : 1 \n Taille disponible :%zu \n", T, T->taille);
+	printf("INITIALISATION :\n Adresse de T : %p \n Taille disponible :%zu \n", T, T->taille);
+	
 }	
 //-----------------------------------------
 //RECHERCHE : Cherche la première zone mémoire libre AC assez grande dans la liste chainée T,
@@ -56,7 +59,7 @@ fb* mem_fit_first(fb* T, size_t size){
 		
 		fb* AC = T; //Le FB actuel
 			
-		while((AC != NULL)&&((AC->taille) <= size+sizeof(ub))){
+		while((AC != NULL)&&((AC->taille) < size+sizeof(ub))){
 			AC = AC->next;
 		}			
 		
@@ -70,7 +73,7 @@ fb* mem_fit_best(fb* T, size_t size){
 		
 		while(AC != NULL){
 
-			if(((AC->taille)>size+sizeof(ub))&&(AC->taille<size_min)){
+			if(((AC->taille)>=size+sizeof(ub))&&(AC->taille<size_min)){
 				minAC = AC;
 				size_min = AC->taille;
 			}
@@ -87,7 +90,7 @@ fb* mem_fit_worst(fb* T, size_t size){
 		size_t size_max = T->taille;
 		
 		while(AC != NULL){
-			if(((AC->taille)>size+sizeof(ub))&&(AC->taille>size_max)){
+			if(((AC->taille)>=size+sizeof(ub))&&(AC->taille>size_max)){
 				maxAC = AC;
 				size_max = AC->taille;
 			}
@@ -111,6 +114,7 @@ void* mem_alloc(size_t S) {
 	ub * newU; //La future zone à allouer
 	fb* AC = T; //Le FB actuel
 	fb* AP = T; //le FB précédent
+	size_t zoneMorte = 0; //voir ETAPE 2, cas 2
 	
 	printf("---------------------------------\n");
 	printf("ALLOCATION DE %zu OCTETS (+%lu octets pour UB) :\n", S, sizeof(ub));
@@ -118,7 +122,7 @@ void* mem_alloc(size_t S) {
 	//ETAPE 1 ET 2 : Chercher une zone libre et remanipuler la liste chainée.
 	size_to_alloc = S;
 	newU = (ub*)mem_fit(fonction_recherche); //retourne l'adresse où écrire newU
-	printf("\nnewU trouvé : %p\n\n", newU);
+	
 	//ETAPE 2 : On change la liste chainée pour exclure AC (qui sera remplacé par UB+ZONE_ALLOUEE)
 		while((AC !=NULL)&&(AC != (fb*)newU)){
 			AP = AC;
@@ -127,28 +131,26 @@ void* mem_alloc(size_t S) {
 		
 		if(AC != NULL){
 			
-			printf("\nTAILLE AC vs SIZE+UB :%zu   %zu)\n", AC->taille, S+sizeof(ub));
-			
 			//CAS 1 : cas où on doit créer un nouveau FB en fin de liste chainée
 			if(AC->next == NULL){
 				//on décale le pointeur AC pour laisser de la place à l'allocation
-				char* newAC = (char*)AC+sizeof(ub)+S+1; 
+				char* newAC = (char*)AC+sizeof(ub)+S; 
 				
 				//création de FB
 				fb* newFB = (fb*)newAC;
 				newFB->taille = (AC->taille)-sizeof(ub)-S; 
 				newFB->next=NULL;
-				printf("Nouveau FB : %p (de taille %zu)\n", newFB, newFB->taille);
 				
 				//redirection de la chaine
 				if(AC == T){
 					T = newFB;
 					fb **mem0 = (fb **)M.mem; 
 					*mem0 = T;
-					printf("Nouveau T : %p\n", *(fb **)M.mem);
+					printf("Nouveau T : %p (de taille %zu)\n", *(fb **)M.mem, T->taille);
 				}
 				else{
 					AP->next = newFB;
+					printf("Nouveau FB : %p (de taille %zu)\n", newFB, newFB->taille);
 				}
 				
 			}
@@ -157,27 +159,29 @@ void* mem_alloc(size_t S) {
 				//on vérifie dans la zone libre qu'il y a assez de place pour créer un nouveau FB
 				if((AC->taille) > S+sizeof(ub)+sizeof(fb)){
 						printf("[Construction de FB en milieu de chaine !] \n");
-						char* newAC = (char*)AC+sizeof(ub)+S+1; 
+						char* newAC = (char*)AC+sizeof(ub)+S; 
 						
 						fb* newFB = (fb*)newAC;
 						newFB->taille = (AC->taille)-sizeof(ub)-S; 
 						newFB->next=AC->next;
-						printf("Nouveau FB : %p (de taille %zu)\n", newFB, newFB->taille);
 						
 						if(AC == T){
 							T = newFB;
 							fb **mem0 = (fb **)M.mem; 
 							*mem0 = T;
-							printf("Nouveau T : %p\n", *(fb **)M.mem);
+							printf("Nouveau T : %p (de taille %zu)\n", *(fb **)M.mem, T->taille);
 						}
 						else{
 							AP->next = newFB;
+							printf("Nouveau FB : %p (de taille %zu)\n", newFB, newFB->taille);
 						}
 				}
 				// cas où on prend TOUTE la zone libre
 				else {
 					printf("[La zone allouée prend toute la zone libre !(pas de nouveau FB)] \n");
-					
+					//on rajoute les derniers octets inutiles pour ne pas laisser de zones "mortes"
+					zoneMorte = (AC->taille) - (S+sizeof(ub)); 
+					printf("zone Morte ajoutée : %zu\n", zoneMorte);				
 					if(AC == T){
 							T = AC->next;
 							fb **mem0 = (fb **)M.mem; 
@@ -186,17 +190,20 @@ void* mem_alloc(size_t S) {
 						}
 						else{
 							AP->next = AC->next;
-						}
-					
+						}				
 				}
-				
 			}
+		
+			//ETAPE 3 : On crée UB
+			printf("Nouveau UB : %p\n", newU);
+			newU -> taille = S + zoneMorte; //la taille ne comprend pas UB (contrairement à FB)
+			return ((void*)newU) + sizeof(ub); //On retourne le DEBUT de la zone allouée, UB exclue ! Il ne faut pas laisser l'occasion à l'utilisateur d'écraser UB...
+		}
+		else{
+			printf("ALERTE : Mémoire saturée !\n");
 		}
 	
-	//ETAPE 3 : On crée UB
-	printf("Création de UB : %p\n", newU);
-	newU -> taille = S;
-	return ((void*)newU) + sizeof(ub); //On retourne le DEBUT de la zone allouée, UB exclue ! Il ne faut pas laisser l'occasion à l'utilisateur d'écraser UB...
+	return NULL;
 	
 }
 //-----------------------------------------
@@ -209,13 +216,18 @@ void mem_free(void* m){
 	U = (ub*)(m-sizeof(ub));
 	size_t size = U->taille;
 	
+	if(size > M.taille){
+		printf("ALERTE : Erreur de libération (taille trop grande) \n");
+		return;
+	}
+	
 	//ETAPE 2 : Création du nouveau FB
 	newFB = (fb*)U;
-	newFB -> taille = size + sizeof(ub) - sizeof(fb);
+	newFB -> taille = size+sizeof(ub);
 	newFB->next = NULL;
 	
 	printf("---------------------------------\n");
-	printf("LIBERATION DE %zu OCTETS (+%lu octets pour FB) :\n", size, sizeof(fb));
+	printf("LIBERATION DE %zu OCTETS (+%lu octets pour UB) :\n", size, sizeof(ub));
 	
 	//ETAPE 3 : Chercher la zone libre avant ET après newFB
 	fb * T = *((fb**)M.mem);
@@ -226,9 +238,6 @@ void mem_free(void* m){
 		AP = AC;
 		AC = AC->next;
 	}
-	
-	printf("AP(old) : %p (de taille %zu) pointe vers %p \n", AP, AP->taille, AP->next);
-	printf("AC(old) : %p (de taille %zu) pointe vers %p \n \n", AC, AC->taille, AC->next);
 	
 	//Normalement, à la fin, newFB se trouve entre AC et AP.
 	if(AC != NULL){
@@ -254,14 +263,14 @@ void mem_free(void* m){
 	
 		//Cas où on peut fusionner avec AC	 
 		if ((char*)newFB + (newFB->taille) + sizeof(fb)+1 == (char*)AC){ 
-			newFB->taille = (newFB->taille) + (AC->taille) + sizeof(fb);
+			newFB->taille = (newFB->taille) + (AC->taille);
 			newFB->next = AC->next;
 			printf("\n FUSION AC :\n newFB : %p (de taille %zu) pointe vers %p \n", newFB, newFB->taille, newFB->next);
 		}
 		
 		//Cas où on peut fusionner avec AP
 		if ((char*)AP + (AP->taille) + sizeof(fb)+1 == (char*)newFB){ 
-			AP->taille = (AP->taille) + (newFB->taille) + sizeof(fb);	
+			AP->taille = (AP->taille) + (newFB->taille);	
 			AP->next = newFB->next;
 			printf("\n FUSION AP :\n AP : %p (de taille %zu) pointe vers %p \n", AP, AP->taille, AP->next);
 		}
@@ -270,26 +279,63 @@ void mem_free(void* m){
 	
 }
 //-----------------------------------------
+//Donne la taille qui POURRAIT être réallouer si UB se libérait (regarder les zones libres voisines)
 size_t mem_get_size(void *m){
 	
-	ub* U;
-	U = (ub*)(m-sizeof(ub));
+	ub* U = (ub*)(m-sizeof(ub));
+	size_t reallocU = U->taille;
 	
-	return U->taille;
+	fb * T = *((fb**)M.mem);
+	fb* AC = T; //Le FB actuel
+	fb* AP = T; //le FB précédent
+	
+	while((AC != NULL)&&(AC < (fb*)U)){ //on cherche la zone libre JUSTE avant U
+		AP = AC;
+		AC = AC->next;
+	}
+	
+	if(AC!=NULL){
+			//Cas où on peut fusionner avec AC si U se libérait
+			if ((char*)U+ sizeof(ub)+ (U->taille)+1 == (char*)AC){ 
+				reallocU = (U->taille) + (AC->taille) + sizeof(ub);
+				printf("\n REALLOCATION AVEC AC :\n U : %p (de taille %zu) gagnerait %zu octets \n", U, U->taille, (AC->taille));
+			}
+		
+			//Cas où on peut fusionner avec AP si U se libérait
+			if ((char*)AP + (AP->taille) + sizeof(fb)+1 == (char*)U){ 
+				reallocU = (AP->taille) + (U->taille) + sizeof(fb);	
+				printf("\n REALLOCATION AVEC AC :\n U : %p (de taille %zu) gagnerait %zu octets \n", U, U->taille, (AP->taille));
+			}
+	}
+	
+	return reallocU;
 }
 //-----------------------------------------
 //Ici on va parcourir les zones allouées et libres dans l'ordre
 void mem_show(void (*print)(void *, size_t, int free)){
 
-	/*fb* AC = ((fb *)M.mem)+1; //première zone (libre ou occupée) de la mémoire.
-	fb* derniere_adr_mem = (fb*)(M.mem + M.taille);
+	fb * T = *((fb**)M.mem);
+	fb *AC = T; //FB courant
+	ub* UC=(ub*)(((fb *)M.mem)+1); //UB courant (ici, premier UB de la mémoire)
 	
-	size_t size;*/
+	//ETAPE 1 : Parcourir chaque zone libre un à un. Faire un print pour ces zones
+	while(AC != NULL){
 	
-	/*while(AC < derniere_adr_mem){
-		size = AC->taille;
+		//ETAPE 2: Parcourir les UB AVANT AC
+		while(UC < (ub*)AC){			
+			print((void*)UC, UC->taille, OCCUPEE);
+			UC = (ub*)(((char*)UC) + UC->taille+sizeof(ub));
+		}
+
+		print((void*)AC, AC->taille, LIBRE);
 		
-	}*/
+		if(AC->next != NULL){
+			UC = (ub*)(((char*)AC) + AC->taille);
+		}
+		AC=AC->next;
+		
+	}
+
 		
 }
 
--- MASTER1/SEMESTRE1/CSE/TP2/test_init.c
+++ MASTER1/SEMESTRE1/CSE/TP2/test_init.c
@@ -5,9 +5,16 @@
 #include <assert.h>
 #define NB_TESTS 10
 
+void afficher_zone(void *adresse, size_t taille, int free)
+{
+  printf("Zone %s, Adresse : %lx, Taille : %lu\n", free?"libre":"occupee",
+         (unsigned long) adresse, (unsigned long) taille);
+}
+
+
 int main(int argc, char *argv[]) {
 	
-	void *result, *resultFB;
+	void *result, *resultA, *resultB;
 	
 	fprintf(stderr, "\n\nTest basique de mémoire. J'ai supprimé l'ancien test car pas assez précis.\n"
 			"On va allouer/libérer différentes zones afin de tester les cas possibles.\n T (pour 'tête') correspond au pointeur vers la tête de la liste chainée. \n AC (pour 'Adresse Courante') est le FB pointé lors du parcours de la chaine. \n AP (pour 'Adresse Précédente') est le FB précédant AC. \n newFB est tout simplement l'adresse vers la nouvelle zone libre crée. \n Ces infos vont aider (lors d'une éventuelle présentation) à bien montrer la construction de la mémoire en cours d'exécution.\n \n"
@@ -15,28 +22,39 @@ int main(int argc, char *argv[]) {
 	
 	mem_init(get_memory_adr(), get_memory_size());
 	
-	result = mem_alloc((size_t)1000);
-	debug("Alloced %zu bytes at %p\n", (size_t)1000, result);
+	resultB = mem_alloc((size_t)1000);
+	debug("Alloced %zu bytes at %p\n", (size_t)1000, resultB);
 	
 	result = mem_alloc((size_t)500);
 	debug("Alloced %zu bytes at %p\n", (size_t)500, result);
 	
-	resultFB = mem_alloc((size_t)2000);
-	debug("Alloced %zu bytes at %p\n", (size_t)2000, resultFB);
+	resultA = mem_alloc((size_t)2000);
+	debug("Alloced %zu bytes at %p\n", (size_t)2000, resultA);
 	
 	result = mem_alloc((size_t)400);
 	debug("Alloced %zu bytes at %p\n", (size_t)400, result);
 	
-	mem_free(resultFB);
-	debug("Freeing at %p\n", resultFB);
+	mem_free(resultB);
+	debug("Freeing at %p\n", resultB);
+	
+	mem_free(resultA);
+	debug("Freeing at %p\n", resultA);
+	
+	result = mem_alloc((size_t)6000);
+	debug("Alloced %zu bytes at %p\n", (size_t)6000, result);
+	
+	result = mem_alloc((size_t)1500);
+	debug("Alloced %zu bytes at %p\n", (size_t)1500, result);
 	
 	result = mem_alloc((size_t)500);
 	debug("Alloced %zu bytes at %p\n", (size_t)500, result);
 	
+	result = mem_alloc((size_t)490);
+	debug("Alloced %zu bytes at %p\n", (size_t)490, result);
+	
 	printf("\n---------------------------------\n");
-	printf("TEST MEM_GET_SIZE : la dernière zone allouée fait bien %zu octets. \n", mem_get_size(result));
-	
-	
+	printf("TEST MEM SHOW : \n");
+	mem_show(afficher_zone);
 	printf("\n \nFIN !\n");
 	return 0;
 }
